79b46ab3d02d37df62cb6853f24120a86fa17d25
PeihongKe <pkukph@hotmail.com>
2016-11-14 23:18:31 +0000

    add test for iterator traits

--- astl/iterator/tests/iterator_traits_test.cpp
+++ astl/iterator/tests/iterator_traits_test.cpp
@@ -1,22 +1,55 @@
-
-
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE MyTest 
+#define BOOST_TEST_MODULE iterator_trait
 #include <boost/test/unit_test.hpp>
+#include "astl\iterator\iterator_traits.hpp"
+#include <typeinfo>
+#include <iostream>
+#include <string>
 
+using namespace anotherSTL;
 
-
-
-//int main()
-//{
-//	return 0;
-//}
-//
-
-
-BOOST_AUTO_TEST_CASE(my_test)
+BOOST_AUTO_TEST_CASE(category_function)
 {
-	BOOST_CHECK_EQUAL(1+7, 8);
- 
+	int a[3] = { 1,2,3 };
+	int *begin = a;
+	random_access_iterator_tag* rci(dynamic_cast<random_access_iterator_tag*>(&category(begin)));
+	BOOST_CHECK(rci != nullptr);
 }
+
+BOOST_AUTO_TEST_CASE(iterator_distance_type_function)
+{
+	int a[3] = { 1,2,3 };
+	BOOST_CHECK(std::string(typeid(distance_type(&a[0])).name()) ==  "int *\0");
+}
+
+BOOST_AUTO_TEST_CASE(iterator_value_type_function)
+{
+	int a[3] = { 1,2,3 };
+	std::cout << std::string(typeid(value_type(&a[0])).name());
+	BOOST_CHECK(std::string(typeid(value_type(&a[0])).name()) == "int *\0");
+}
+
+
+BOOST_AUTO_TEST_CASE(iterator_traits_specialization1)
+{
+	BOOST_CHECK(std::string(typeid(iterator_traits<int *>::value_type).name()) == "int\0");
+	BOOST_CHECK(std::string(typeid(iterator_traits<int *>::pointer).name()) == "int *\0");
+	BOOST_CHECK(std::string(typeid(iterator_traits<int *>::reference).name()) == "int\0");
+	BOOST_CHECK(std::string(typeid(iterator_traits<int *>::difference_type).name()) == "int\0");	
+	BOOST_CHECK(std::string(typeid(iterator_traits<int *>::iterator_category).name()) == "struct anotherSTL::random_access_iterator_tag\0");
+}
+
+
+BOOST_AUTO_TEST_CASE(iterator_traits_specialization2)
+{	
+	BOOST_CHECK(std::string(typeid(iterator_traits<const int *>::value_type).name()) == "int\0");
+	BOOST_CHECK(std::string(typeid(iterator_traits<const int *>::pointer).name()) == "int const *\0");	
+	//TODO: check
+	BOOST_CHECK(std::string(typeid(iterator_traits<const int *>::reference).name()) == "int\0");
+
+	BOOST_CHECK(std::string(typeid(iterator_traits<const int *>::difference_type).name()) == "int\0");
+	BOOST_CHECK(std::string(typeid(iterator_traits<const int *>::iterator_category).name()) == "struct anotherSTL::random_access_iterator_tag\0");
+}
+
+
 
