4cafae6d286ccc0dc28eea8987ba469f36b4b5c3
PeihongKe <pkukph@hotmail.com>
2016-11-19 01:03:01 +0000

    add type traits class

--- astl/base/type_traits.hpp
+++ astl/base/type_traits.hpp
@@ -2,6 +2,44 @@
 #define _ANOTHERSTL_TYPE_TRAITS_
 
  
+namespace anotherSTL
+{
+	struct _true_type{};
+	struct _false_type{};
+
+	template<typename T>
+	struct type_traits 
+	{	
+		typedef _false_type is_POD_type;
+		typedef _false_type has_trivial_default_ctr;
+		typedef _false_type has_trivial_copy_ctr;
+		typedef _false_type has_trivial_assignment_ctr;
+		typedef _false_type has_trivial_dtor;
+	};
+
+	// partial specilization
+	template<typename T>
+	struct type_traits<T*>
+	{
+		typedef _true_type is_POD_type;
+		typedef _true_type has_trivial_default_ctr;
+		typedef _true_type has_trivial_copy_ctr;
+		typedef _true_type has_trivial_assignment_ctr;
+		typedef _true_type has_trivial_dtor;
+	};
+
+	template<>
+	struct type_traits<char>
+	{
+		typedef _true_type is_POD_type;
+		typedef _true_type has_trivial_default_ctr;
+		typedef _true_type has_trivial_copy_ctr;
+		typedef _true_type has_trivial_assignment_ctr;
+		typedef _true_type has_trivial_dtor;
+	};
+}
+
+
 
 
 #endif
