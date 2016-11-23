89d69b214301019e7d9b95e70fdfb703a61f9c94
PeihongKe <pkukph@hotmail.com>
2016-11-23 22:36:26 +0000

    add inline for algorithm copy

--- astl/algorithm/modify_algo.hpp
+++ astl/algorithm/modify_algo.hpp
@@ -19,7 +19,7 @@ namespace anotherSTL
 	}	 
 
 	template<typename InputIterator, typename OutputIterator>
-	OutputIterator copy(InputIterator first1, InputIterator last1, OutputIterator first2)
+	inline OutputIterator copy(InputIterator first1, InputIterator last1, OutputIterator first2)
 	{
 		_copy_functor<InputIterator, OutputIterator> copy_f;
 		return copy_f(first1, last1, first2);
@@ -30,7 +30,7 @@ namespace anotherSTL
 	struct _copy_functor
 	{
 
-		OutputIterator operator() (InputIterator first1, InputIterator last1, OutputIterator first2)
+		inline OutputIterator operator() (InputIterator first1, InputIterator last1, OutputIterator first2)
 		{
 			typedef typename iterator_traits<InputIterator>::iterator_category category;
 			return _copy_by_category(first1, last1, first2, category());
@@ -41,7 +41,7 @@ namespace anotherSTL
 	template<typename T>
 	struct _copy_functor<T*, T*>
 	{
-		T* operator() (T* first1, T* last1, T* first2)
+		inline T* operator() (T* first1, T* last1, T* first2)
 		{
 			typedef type_traits<T>::has_trivial_assignment_ctr has_trivial_assignment_ctr;
 			return _copy_by_trivial_assign_ctr_4_ptr(first1, last1, first2, has_trivial_assignment_ctr());
@@ -52,7 +52,7 @@ namespace anotherSTL
 	template<typename T>
 	struct _copy_functor<const T*, T*>
 	{
-		T* operator() (const T* first1, const T* last1, T* first2)
+		inline T* operator() (const T* first1, const T* last1, T* first2)
 		{
 			typedef type_traits<T>::has_trivial_assignment_ctr has_trivial_assignment_ctr;
 			return _copy_by_trivial_assign_ctr_4_ptr(first1, last1, first2, has_trivial_assignment_ctr());
@@ -61,7 +61,7 @@ namespace anotherSTL
 
 	// trivial assignment operator (iterators are points only now)
 	template<typename T>
-	T* _copy_by_trivial_assign_ctr_4_ptr(T* first1, T* last1, T* first2, _true_type)
+	inline T* _copy_by_trivial_assign_ctr_4_ptr(T* first1, T* last1, T* first2, _true_type)
 	{	
 		size_t size = last1 - first1;
 		memmove(first2, first1, sizeof(T)* size);
@@ -70,13 +70,13 @@ namespace anotherSTL
 
 	// non-trivial assignment operator (iterators are points only now)
 	template<typename T>
-	T* _copy_by_trivial_assign_ctr_4_ptr(T* first1, T* last1, T* first2, _false_type)
+	inline T* _copy_by_trivial_assign_ctr_4_ptr(T* first1, T* last1, T* first2, _false_type)
 	{
 		return _copy_by_category(first1, last1, first2, random_access_iterator_tag());
 	}
 
 	template<typename InputIterator, typename OutputIterator>
-	OutputIterator _copy_by_category(InputIterator first1, InputIterator last1, OutputIterator first2, random_access_iterator_tag)
+	inline OutputIterator _copy_by_category(InputIterator first1, InputIterator last1, OutputIterator first2, random_access_iterator_tag)
 	{
 		for (ptrdiff_t i = 0; i < last1 - first1; ++i, ++first2)
 		{
@@ -86,7 +86,7 @@ namespace anotherSTL
 	}
 
 	template<typename InputIterator, typename OutputIterator>
-	OutputIterator _copy_by_category(InputIterator first1, InputIterator last1, OutputIterator first2, input_interator_tag)
+	inline OutputIterator _copy_by_category(InputIterator first1, InputIterator last1, OutputIterator first2, input_interator_tag)
 	{
 		for (; first1 != last1; ++first1, ++first2)
 		{
@@ -94,6 +94,7 @@ namespace anotherSTL
 		}
 		return first2;
 	}
+
 
 
 
