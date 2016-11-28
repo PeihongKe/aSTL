d3c9d77c8e19877c224b7d28fce79c0b00a4d745
PeihongKe <pkukph@hotmail.com>
2016-11-28 23:17:53 +0000

    relocate the code of simpleAllocator to its own file

--- astl/allocator/simpleAllocator.hpp
+++ astl/allocator/simpleAllocator.hpp
@@ -75,6 +75,34 @@ namespace anotherSTL
 
 	};
 
+	template<typename T, class Alloc>
+	class simple_alloc
+	{
+	public:
+		static T* allocate(size_t n)
+		{
+			return 0 == n ? 0 : Alloc::allocate(n * sizeof(T));
+		}
+
+		static T* allocate(void)
+		{
+			return Alloc::allocate(sizeof(T));
+		}
+
+		static void deallocate(T *p, size_t n)
+		{
+			if (0 != n)
+			{
+				Alloc::deallocate(p, n * sizeof(n));
+			}
+		}
+
+		static void deallocate(T *p)
+		{
+			Alloc::deallocate(p, sizeof(T));
+		}
+	};
+
 }
 
 #endif
