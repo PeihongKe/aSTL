889871ee27f7af231c04d23ac9ee4e9b1751927d
PeihongKe <pkukph@hotmail.com>
2017-06-12 22:39:37 +0100

    add list interface for 98

--- astl/container/list.hpp
+++ astl/container/list.hpp
@@ -1,6 +1,8 @@
 #ifndef _ANOTHERSTL_LIST_
 #define _ANOTHERSTL_LIST_
 #include "astl\iterator\iterator_traits.hpp"
+#include "astl\allocator\simpleAllocator.hpp"
+#include "astl\iterator\iterator.hpp"
 
 namespace anotherSTL
 {
@@ -16,15 +18,15 @@ namespace anotherSTL
 	template<typename T, class Ref, class Ptr>
 	class __list_iterator
 	{
-		typedef __list_iterator<T, T&, T*> iterator;
-		typedef __list_iterator<T, Ref, Ptr> self;
-		typedef bidirectional_iterator_tag iterator_category;
-		typedef T value_type;
-		typedef Ptr pointer;
-		typedef Ref reference;
-		typedef size_t size_type;
-		typedef ptrdiff_t difference_type;
-		typedef __list_node<T>* link_type;
+		typedef __list_iterator<T, T&, T*>			iterator;
+		typedef __list_iterator<T, Ref, Ptr>		self;
+		typedef bidirectional_iterator_tag			iterator_category;
+		typedef T									value_type;
+		typedef Ptr									pointer;
+		typedef Ref									reference;
+		typedef size_t								size_type;
+		typedef ptrdiff_t							difference_type;
+		typedef __list_node<T>*						link_type;
 
 		link_type node;
 
@@ -63,6 +65,336 @@ namespace anotherSTL
 			--*this;
 			return temp;
 		}
+	};
+
+	template<typename T, typename Alloc = simpleAllocator<T> >
+	class list
+	{
+
+	public:
+		typedef T											value_type;
+		typedef Alloc										allocator_type;
+
+		// as the first attemp just define the default member types for default Alloc
+		typedef T&											reference;          // this should be allocator_type::reference
+		typedef const T&									const_reference;    // this should be allocator_type::const_reference
+		typedef T*											pointer;            // this should be allocator_type::pointer
+		typedef const T*									const_pointer;      // this should be allocator_type::const_pointer
+		typedef __list_iterator<T, T&, T*		>			iterator;
+		typedef const __list_iterator<T, T&, T*>			const_iterator;
+		// todo : typedef reverse_iterator
+		// todo : typedef const_reverse_iterator
+		typedef size_t										size_type;
+		typedef iterator_traits<iterator>::difference_type	difference_type;
+
+		// ctr.
+		explicit list(const allocator_type& alloc = allocator_type())
+		{
+
+		}
+
+		explicit list(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+		{
+
+		}
+
+		template<class InputIterator>
+		list(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+		{
+
+		}
+
+		list(const list& x)
+		{
+
+		}
+
+		~list()
+		{
+
+		}
+
+		// iterator
+		iterator begin()
+		{
+
+		}
+
+		const_iterator begin() const
+		{
+
+		}
+
+		iterator end()
+		{
+
+		}
+
+		const_iterator end() const
+		{
+
+		}
+
+		list& operator=(const list& x)
+		{
+
+		}
+
+
+		//reverse_iterator rbegin()
+		//{
+
+		//}
+
+		//const_reverse_iterator rbegin() const
+		//{
+
+		//}
+
+		// capacity.
+		bool empty() const
+		{
+
+		}
+
+		size_type size() const
+		{
+
+		}
+
+		size_type max_size() const
+		{
+
+		}
+
+		//element access
+		reference front()
+		{
+
+		}
+
+		const_reference front() const
+		{
+
+		}
+
+		reference back()
+		{
+
+		}
+
+		const_reference back() const
+		{
+
+		}
+
+		// modifier
+		template<InputIterator>
+		void assign(InputIterator first, InputIterator last)
+		{
+
+		}
+
+		void assign(size_type n, const value_type& val)
+		{
+
+		}
+
+		void push_front(const value_type& val)
+		{
+
+		}
+
+		void push_back(const value_type& val)
+		{
+
+		}
+
+		void pop_back()
+		{
+
+		}
+
+		void push_back()
+		{
+
+		}
+
+		// return value: point to the inserted value
+		iterator insert(iterator pos, const value_type& val)
+		{
+
+		}
+
+
+		void insert(iterator pos, size_type n, const value_type& val)
+		{
+
+		}
+
+		template<class InputIterator>
+		void insert(iterator pos, InputIterator first, InputIterator last)
+		{
+
+		}
+
+		iterator erase(const_iterator pos)
+		{
+
+		}
+
+		iteartor erase(const_iterator first, const_iterator last)
+		{
+
+		}
+
+		void swap(list& x)
+		{
+
+		}
+
+		void resize(size_type n, const value_type& val = value_type())
+		{
+
+		}
+
+		void clear()
+		{
+
+		}
+
+		// operation
+		void splice(iterator pos, list&x)
+		{
+
+		}
+
+		void splice(iterator pos, list&x, iterator i)
+		{
+
+		}
+
+		void splice(iterator pos, list& x, iterator first, iterator last)
+		{
+
+		}
+
+		void remove(const value_type& val)
+		{
+
+		}
+
+		template<typename Predicate>
+		void remove_if(Predict pred)
+		{
+
+		}
+
+		void unique()
+		{
+
+		}
+
+		template<class BinaryPredicate>
+		void unique(BinaryPredicate binary_pred)
+		{
+
+		}
+
+		void merge(list& x)
+		{
+
+		}
+
+		template<typename Compare>
+		void merge(list& x, Compare comp)
+		{
+
+		}
+
+		void sort()
+		{
+
+		}
+
+		template<typename Compare>
+		void sort(Compare comp)
+		{
+
+		}
+
+		void reverse()
+		{
+
+		}
+
+		//observors
+		allocator_type get_allocator() const
+		{
+
+		}
+
+		// non-member functions
+		template<typename T, typename Alloc>
+		bool operator==(const list<T, Alloc>& lhs, const list<T, Alloc>& rhs)
+		{
+
+		}
+
+		template<typename T, typename Alloc>
+		bool operator!=(const list<T, Alloc>& lhs, const list<T, Alloc>& rhs)
+		{
+			return !(lhs == rhs);
+		}
+
+		template<typename T, typename Alloc>
+		bool operator<(const list<T, Alloc>& lhs, const list<T, Alloc>& rhs)
+		{
+
+		}
+
+		template<typename T, typename Alloc>
+		bool operator>=(const list<T, Alloc>& lhs, const list<T, Alloc>& rhs)
+		{
+			return !(lhs < rhs);
+		}
+
+		template<typename T, typename Alloc>
+		bool operator>(const list<T, Alloc>& lhs, const list<T, Alloc>& rhs)
+		{
+			return !(lhs <= rhs);
+		}
+
+		template<typename T, typename Alloc>
+		bool operator<=(const list<T, Alloc>& lhs, const list<T, Alloc>& rhs)
+		{
+			return (lhs < rhs) && lhs == rhs;
+		}
+
+
+	
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	};
 
 }
